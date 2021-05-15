66a5aa648d406234d5300595d894650a74117692
kaveeshwa-iil <kaveeshwa.b@informaticsint.com>
2021-05-15 12:50:37 +0530

    Updates

---
+++ precache-manifest.9e172b4737e150b1434377a8bd362868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05dcb72d8656948e151cbb325df32c70",
+    "url": "/birthday-wish/index.html"
+  },
+  {
+    "revision": "b6e3dd45f9ad790378ec",
+    "url": "/birthday-wish/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-wish/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-wish/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e3dd45f9ad790378ec",
+    "url": "/birthday-wish/static/js/main.52c9ba9b.chunk.js"
+  },
+  {
+    "revision": "a26c12b6edd2be553dc1",
+    "url": "/birthday-wish/static/js/runtime-main.6a2dd9ba.js"
+  }
+]);
